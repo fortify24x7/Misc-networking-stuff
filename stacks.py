f396f7d55fdd51fac0dc566694e81ca56796f728
RussianOtter <elangs321@gmail.com>
2017-06-19 21:35:05 -0400

    Disassembler And Memory ID Viewer

---
+++ stacks.py
@@ -0,0 +1,25 @@
+# Move file to 'site-packages'
+def func_bytes(fl):
+	import console, dis
+	"""
+	This function reads a script and
+	prints it's memory address.
+	"""
+	f = open(fl).readlines()
+	for line in f:
+		try:
+			co = compile(line,"<none>","exec")
+			t = str(str(co).split("at ")[1]).split(",")[0]
+			print t, co.consts
+		except:
+			pass
+	time.sleep(2)
+	console.set_font("Menlo",10)
+	f = open(fl).readlines()
+	for line in f:
+		try:
+			dis.dis(line)
+			time.sleep(0.001)
+		except:
+			pass
+	console.set_font()
