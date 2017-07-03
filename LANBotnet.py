68d56188afd54e8923ca32365770b021ffe9b225
RussianOtter <elangs321@gmail.com>
2017-07-03 02:38:31 -0400

    v1.3

--- LANBotnet.py
+++ LANBotnet.py
@@ -1,8 +1,8 @@
 import socket, time, random, string, sys, argparse
 
-import argparse, sys
 parser = argparse.ArgumentParser()
 parser.add_argument("-v", "--verbose",help="Server Verbose", action="store_true")
+parser.add_argument("-N", "--Name",help="Server Name", required=False)
 parser.add_argument("-s", "--servers",help="Make Random Servers", type=int, default=0)
 parser.add_argument("-n", "--nohelp",help="Disable Help", action="store_true")
 args = parser.parse_args()
@@ -13,35 +13,43 @@ if not args.nohelp:
 
 verbose = args.verbose
 srvs = args.servers
+named = args.Name
 
 servers = [
-	[" End Of School Year! I Made 1000 Servers!", 420]
+	["Test Server", 420],
+	["Test Server 2", 720],
+	["Test Server 3", 360]
 ]
 
 if srvs > 0:
 	servers = []
-	for i in range(1,srvs):
-		servers.append([str(i+1) + " " + "".join(random.sample(string.ascii_letters,15)), random.choice(range(10000,31000))])
+	for i in range(1,srvs+1):
+		if named:
+			servers.append([named+str(i),random.choice(range(10000,31000))])
+		else:
+			servers.append([str(i+1) + " " + "".join(random.sample(string.ascii_letters,15)), random.choice(range(10000,31000))])
 
-BROADCAST_IP = "255.255.255.255"
-BROADCAST_PORT = 4445
+ip = "255.255.255.255"
+port = 4445
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-sock.bind(("",BROADCAST_PORT))
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind(("",port))
 socket.setdefaulttimeout(1)
+time.sleep(0.5)
 
-print "\n - Broadcasting Minecraft Servers - \n"
+print "\n - Broadcasting Servers - \n"
 for i in range(len(servers)):
 	sys.stdout.write("\rTotal Servers: "+str(i+1))
+	time.sleep(0.005)
 if verbose:
 	print "\n"
-	for s in servers:
-		print s
+	for _ in servers:
+		print _
 else:
-	for s in servers:
-		sys.stdout.write("\r"+str(s))
-		time.sleep(100/len(servers))
+	for _ in servers:
+		sys.stdout.write("\r"+str(_))
+		time.sleep(0.2)
 	print ""
 
 def checksum():
@@ -55,21 +63,20 @@ def checksum():
 	try:
 		for server in servers:
 			msg = "[MOTD]%s[/MOTD][AD]%d[/AD]" % (server[0], server[1])
-			sock.sendto(msg, (BROADCAST_IP, BROADCAST_PORT))
-			data.append(sock.recv(100))
+			s.sendto(msg, (ip, port))
+			data.append(s.recv(100))
 			found.append(msg)
 			time.sleep(0.005)
 		for c in found:
 			if c in data:
 				a = a + 1
-				sys.stdout.write("\r[+] Confirmed %s of %s Servers" %(a+1,len(servers)+1))
-				time.sleep(0.005)
+				sys.stdout.write("\r[+] Confirmed %s of %s Servers" %(a+1,len(servers)))
 	except:
 		pass
-	sys.stdout.write("\r[+] Confirmed %s of %s Servers\n" %(a+2,len(servers)+1))
+	sys.stdout.write("\r[+] Confirmed %s of %s Servers\n" %(a+1,len(servers)))
 	if a == 0:
 		print "[-] Exiting - No Connection"
-		sock.close()
+		s.close()
 		sys.exit()
 	elif verbose:
 		print " - Missing Servers - "
@@ -87,10 +94,10 @@ while 1:
 	for server in servers:
 		try:
 			msg = "[MOTD]%s[/MOTD][AD]%d[/AD]" % (server[0], server[1])
-			sock.sendto(msg, (BROADCAST_IP, BROADCAST_PORT))
+			s.sendto(msg, (ip, port))
 		except KeyboardInterrupt:
 			print "\n[+] Stopping Servers"
-			sock.close()
+			s.close()
 			sys.exit()
 		except:
 			b = b + 1
@@ -99,4 +106,4 @@ while 1:
 		if check:
 			checksum()
 		check = False
-	time.sleep(1.5)
+	time.sleep(1.7)
